f5c955b169e1c3caa2983cb621f01126add0883e
Abby Miller <abbymiller@wireless-10-104-2-122.umd.edu>
2021-10-28 16:46:44 -0400

    deleted unecessary parts

--- project.py
+++ project.py
@@ -1,13 +1,7 @@
 """ This is our final project """
 
-class Project:
-    """Class for our project."""
-    def method(): 
-        pass
-    # hi
-    # hello
-
 class Convert:
     """Class for converting currency."""
 
     def USD_EUR():
+        pass
