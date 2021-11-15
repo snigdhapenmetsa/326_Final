356f6160dd1d4896d87dd56bb1d1d7eeae2a5673
Zoey Anastasiadis <zcanast@gmail.com>
2021-11-15 13:33:24 -0500

    fixed init methods

--- currency_converter.py
+++ currency_converter.py
@@ -10,18 +10,10 @@ class Person:
         membership (boolean)
     """
     
-    def __init__(self, filepath):
-        self.people = {}
-        
-        with open(filepath, "r", encoding="utf-8") as f:
-            for line in f:
-                
-                self.name, self.balance, self.membership = line.split(",")
-                self.balance = float(self.balance)
-                self.membership = self.membership.strip()
-                
-                self.people[self.name] = (self.balance, self.membership)
-                print(self.name, self.balance, self.membership)
+    def __init__(self, name, balance, membership):
+        self.name = name
+        self.balance = balance
+        self.membership = membership
 
 class Currency_Shop:
     """Class for converting currency.
@@ -30,19 +22,28 @@ class Currency_Shop:
         items (list of str)
     """
     
-    def __init__(self, filepath):
+    def __init__(self, person_file, item_file):
+        self.people = {}
+        
+        with open(person_file, "r", encoding="utf-8") as f:
+            for line in f:
+                
+                self.name, self.balance, self.membership = line.split(",")
+                self.balance = float(self.balance)
+                self.membership = self.membership.strip()
+                
+                self.people[self.name] = (self.balance, self.membership)
+        
         self.items = list()
         
-        with open(filepath, "r", encoding="utf-8") as f:
+        with open(item_file, "r", encoding="utf-8") as f:
             for line in f: 
                 self.items.append(line.strip())
     
 def main(person_file, item_file):
-    person = Person(person_file)
-    item = Currency_Shop(item_file)
+    transaction = Currency_Shop(person_file, item_file)
             
 def parse_args(arglist):
-
     parser = ArgumentParser()
     parser.add_argument("person_file", help="path to txt file containing people")
     parser.add_argument("item_file", help="path to txt file containing items")
