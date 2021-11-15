ca841458b90cb9d75cf0618bba1e75d924afaecb
Zoey Anastasiadis <zcanast@gmail.com>
2021-11-14 20:48:59 -0500

    added to methods

--- currency_converter.py
+++ currency_converter.py
@@ -10,9 +10,31 @@ class Person:
         self.name = name
         self.balance = balance
         self.membership = membership
+        
+    def membership_discount(self):
+        if self.membership == "silver":
+            return 10
+        elif self.membership == "gold":
+            return 15
+        elif self.membership == "platinum":
+            return 20
 
 class Currency_Shop:
-    """Class for converting currency."""
+    """Class for converting currency.
+    
+    Attributes:
+        filepath (str)
+    """
      
     def __init__(self, filepath):
-        pass
+        self.people = {}
+        
+        with open(filepath, "r", encoding="utf-8") as f:
+            for line in f:
+                #people = {}
+                
+                self.name, self.balance, self.membership = line.split(",")
+                self.balance = float(self.balance)
+                self.membership = self.membership.strip()
+                
+                self.people[self.name] = (self.balance, self.membership)
