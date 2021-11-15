205e7ec5d2bf325b4e32801fb5ee68b2e6e3662a
Zoey Anastasiadis <zcanast@gmail.com>
2021-11-15 12:51:42 -0500

    edited init in both classes

--- currency_converter.py
+++ currency_converter.py
@@ -10,14 +10,6 @@ class Person:
         self.name = name
         self.balance = balance
         self.membership = membership
-        
-    def membership_discount(self):
-        if self.membership == "silver":
-            return 10
-        elif self.membership == "gold":
-            return 15
-        elif self.membership == "platinum":
-            return 20
 
 class Currency_Shop:
     """Class for converting currency.
@@ -31,7 +23,6 @@ class Currency_Shop:
         
         with open(filepath, "r", encoding="utf-8") as f:
             for line in f:
-                #people = {}
                 
                 self.name, self.balance, self.membership = line.split(",")
                 self.balance = float(self.balance)
